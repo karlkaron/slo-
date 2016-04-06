f917185d18d9b96e7c42b56d7750e147f409cf1a
karlkaron <gcabac@gmail.com>
2016-04-06 17:46:03 +0300

    versiunea 06.041

---
+++ api/models/News.js
@@ -0,0 +1,29 @@
+/**
+ * News.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    title: {
+      type: 'string',
+      required: true
+    },
+    data: {
+      type: 'string',
+      required: true
+    },
+    anons: {
+      type: 'string',
+      required: true
+    },
+    text: {
+      type: 'string',
+      required: true
+    },
+
+  }
+};
