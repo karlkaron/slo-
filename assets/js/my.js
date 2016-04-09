299b6eb79410d07bf890e8d40c898db52e72e80c
karlkaron <gcabac@gmail.com>
2016-04-09 17:37:17 +0300

    versiunea 08.043

--- assets/js/my.js
+++ assets/js/my.js
@@ -1,32 +1,32 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-        var $menu = $("#menus");
+  var $menu = $("#menus");
 
-        $(window).scroll(function(){
-            if ( $(this).scrollTop() > 100 && $menu.hasClass("default") ){
-                $menu.fadeOut('fast',function(){
-                    $(this).removeClass("default")
-                           .addClass("fixed isFixed")
-                           .fadeIn('fast');
-                });
-            } else if($(this).scrollTop() <= 100 && $menu.hasClass("fixed")) {
-                $menu.fadeOut('fast',function(){
-                    $(this).removeClass("fixed isFixed")
-                           .addClass("default")
-                           .fadeIn('fast');
-                });
-            }
-        });//scroll
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100 && $menu.hasClass("default")) {
+      $menu.fadeOut('fast', function() {
+        $(this).removeClass("default")
+          .addClass("fixed isFixed")
+          .fadeIn('fast');
+      });
+    } else if ($(this).scrollTop() <= 100 && $menu.hasClass("fixed")) {
+      $menu.fadeOut('fast', function() {
+        $(this).removeClass("fixed isFixed")
+          .addClass("default")
+          .fadeIn('fast');
+      });
+    }
+  }); //scroll
 
-        $menu.hover(
-            function(){
-                if( $(this).hasClass('fixed') ){
-                    $(this).removeClass('transbg');
-                }
-            },
-            function(){
-                if( $(this).hasClass('fixed') ){
-                    $(this).addClass('transbg');
-                }
-            });//hover
-    });
+  $menu.hover(
+    function() {
+      if ($(this).hasClass('fixed')) {
+        $(this).removeClass('transbg');
+      }
+    },
+    function() {
+      if ($(this).hasClass('fixed')) {
+        $(this).addClass('transbg');
+      }
+    }); //hover
+});
